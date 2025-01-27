48eb527499ec01e90c3b45d894de3d6bf5d5083a
slowingwand28 <hum24004@byui.edu>
2025-01-26 19:48:45 -0700

    Finished tutorial as is in video

--- 2d-project-start/2d-project-start/Scenes/player.gd
+++ 2d-project-start/2d-project-start/Scenes/player.gd
@@ -1,7 +1,12 @@
 extends CharacterBody2D
 
+signal health_depleted
 
-func _physics_process(_delta):
+var max_health = 100.0
+var health = 100.0
+var kill_count = 0
+
+func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * 600
 	move_and_slide()
@@ -10,3 +15,11 @@ func _physics_process(_delta):
 		$HappyBoo.play_walk_animation()
 	else:
 		$HappyBoo.play_idle_animation()
+	
+	const DAMAGE_RATE = 5.0
+	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
+	if overlapping_mobs.size() > 0:
+		health -= DAMAGE_RATE * overlapping_mobs.size() * delta
+		%ProgressBar.value = health
+		if health <= 0.0:
+			health_depleted.emit()
