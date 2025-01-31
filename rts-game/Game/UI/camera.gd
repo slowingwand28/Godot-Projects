f19e4889828ff6e93ab65ed375c4b2830f8be993
slowingwand28 <hum24004@byui.edu>
2025-01-31 16:17:29 -0700

    Began work on RTS camera/selection system

---
+++ rts-game/Game/UI/camera.gd
@@ -0,0 +1,41 @@
+extends Camera2D
+
+var mousePos = Vector2()
+var mousePosGlobal = Vector2()
+var start = Vector2()
+var startV = Vector2()
+var end = Vector2()
+var endV = Vector2()
+var isDragging = false
+signal area_selected
+signal start_move_selection
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("LeftClick"):
+		start = mousePosGlobal
+		startV = mousePos
+		isDragging = true
+	if isDragging:
+		end = mousePosGlobal
+		endV = mousePos
+		draw_area()
+	if Input.is_action_just_released("LeftClick"):
+		if startV.direction_to(mousePos) > 20:
+			end = mousePosGlobal
+			endV = mousePos
+			isDragging = false
+			draw_area(false)
+			area_selected.emit()
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouse:
+		mousePos = event.position
+		mousePosGlobal = get_global_mouse_position()
+
+func draw_area(s = true):
+	$"../Panel".size = Vector2(abs(startV.x - endV.x), abs(startV.y - endV.y))
+	var pos = Vector2()
+	pos.x = min(startV.x, endV.x)
+	pos.y = min(startV.y, endV.y)
+	$"../Panel".position = pos
+	$"../Panel".size *= int(s)
