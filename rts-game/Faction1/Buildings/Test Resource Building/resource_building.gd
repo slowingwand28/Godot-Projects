45721d31e3921ef0144ecf12d105d571de904029
slowingwand28 <hum24004@byui.edu>
2025-02-12 16:18:53 -0700

    resource building adds resources on set intervals

--- rts-game/Faction1/Buildings/Test Resource Building/resource_building.gd
+++ rts-game/Faction1/Buildings/Test Resource Building/resource_building.gd
@@ -1,1 +1,24 @@
 extends StaticBody2D
+
+@onready var bar = $ProgressBar
+@onready var timer = $Timer
+var maxTime = 100
+var currTime
+
+func _ready() -> void:
+	currTime = 0
+	bar.max_value = maxTime
+	timer.start()
+
+func _process(delta: float) -> void:
+	if currTime >= maxTime:
+		resourceCollected()
+
+func _on_timer_timeout() -> void:
+	currTime += 1
+	var tween = get_tree().create_tween()
+	tween.tween_property(bar, "value", currTime, 0.5).set_trans(Tween.TRANS_LINEAR)
+
+func resourceCollected():
+	Game.resorces += 10
+	_ready()
