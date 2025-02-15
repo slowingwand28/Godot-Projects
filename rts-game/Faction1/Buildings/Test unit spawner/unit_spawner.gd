3344a475a5142d1e51918ca3b28ba2858feb6c67
slowingwand28 <hum24004@byui.edu>
2025-02-15 16:50:35 -0700

    Revert "experimented with unit spawn. REVERT"
    This reverts commit 9c2a9ffd22be7d90e47a65735eb46a3e98c7fb11.

--- rts-game/Faction1/Buildings/Test unit spawner/unit_spawner.gd
+++ rts-game/Faction1/Buildings/Test unit spawner/unit_spawner.gd
@@ -6,30 +6,17 @@ var mouseEntered = false
 @onready var timer = $Timer
 var selected = false
 var maxTime = 100
-var currTime = 0
-var que = 0
+var currTime
+
 @onready var menu = preload("res://Game/Menus/unit_spawn_menu.tscn")
 @onready var unit = preload("res://Faction1/Units/Test unit/unit.tscn")
 
-func _ready() -> void:
-	startTimer()
-
 func _process(delta: float) -> void:
 	select.visible = selected
-	#if que > 0:
-		#startTimer()
-		#que -= 1
-		#print(str(que))
 	if currTime >= maxTime:
-		timer.stop()
-		if que > 0:
-			spawnUnit()
-			startTimer()
-			que -= 1
 		timer.stop()
 		bar.visible = false
 		spawnUnit()
-
 
 func _input(event: InputEvent) -> void:
 	if (event.is_action_pressed("LeftClick")) and (mouseEntered == true):
@@ -44,11 +31,12 @@ func _on_mouse_exited() -> void:
 	mouseEntered = false
 
 func openMenu():
-	if not self.has_node("Unit Spawn Menu"):
+	var path = get_tree().get_root().get_node("World/UI")
+	if not path.has_node("Unit Spawn Menu"):
 		var spawnMenu = menu.instantiate()
-		#spawnMenu.housePos = position
-		menu.spawn.connect(_on_spawn())
-		self.add_child(spawnMenu)
+		spawnMenu.housePos = position
+		path.add_child(spawnMenu)
+		spawnMenu.yes_pressed.connect(startTimer())
 
 func startTimer():
 	currTime = 0
@@ -59,9 +47,6 @@ func _on_timer_timeout() -> void:
 	currTime += 1
 	var tween = get_tree().create_tween()
 	tween.tween_property(bar, "value", currTime, 0.5).set_trans(Tween.TRANS_LINEAR)
-
-func _on_spawn():
-	pass
 
 func spawnUnit():
 	pass
