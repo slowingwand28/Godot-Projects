ca4bc022dcb297e51e9e87632b288367250ec431
slowingwand28 <hum24004@byui.edu>
2025-02-18 16:14:24 -0700

    got unit que working!!!

--- rts-game/Faction1/Buildings/Test unit spawner/unit_spawner.gd
+++ rts-game/Faction1/Buildings/Test unit spawner/unit_spawner.gd
@@ -7,16 +7,22 @@ var mouseEntered = false
 var selected = false
 var maxTime = 100
 var currTime = 0 
+var que = 0
+var spawning = false
 
 @onready var menu = preload("res://Game/Menus/unit_spawn_menu.tscn")
 @onready var unit = preload("res://Faction1/Units/Test unit/unit.tscn")
 
 func _process(delta: float) -> void:
 	select.visible = selected
+	
+	if (que > 0) and (spawning == false):
+		spawning = true
+		startTimer()
+		
 	if currTime >= maxTime:
-		timer.stop()
 		spawnUnit()
-		bar.visible = false
+		spawning = false
 
 func _input(event: InputEvent) -> void:
 	if (event.is_action_pressed("LeftClick")) and (mouseEntered == true):
@@ -34,12 +40,13 @@ func openMenu():
 	var path = get_tree().get_root().get_node("World/UI")
 	if not path.has_node("Unit Spawn Menu"):
 		var spawnMenu = menu.instantiate()
-		spawnMenu.housePos = position
+		spawnMenu.connect("spawn", signal_recieved)
 		path.add_child(spawnMenu)
 
+func signal_recieved():
+	que += 1
 
 func startTimer():
-	currTime = 0
 	bar.visible = true
 	timer.start()
 
@@ -49,4 +56,18 @@ func _on_timer_timeout() -> void:
 	tween.tween_property(bar, "value", currTime, 0.5).set_trans(Tween.TRANS_LINEAR)
 
 func spawnUnit():
-	print("Spawn Unit")
+	timer.stop()
+	currTime = 0
+	bar.visible = false
+	
+	var randomPosX = randi_range(-100, 100)
+	var randomPosY = randi_range(-100, 100)
+	
+	var path = get_tree().get_root().get_node("World/Friendly Units")
+	var worldPath = get_tree().get_root().get_node("World")
+	var newUnit = unit.instantiate()
+	newUnit.position = Vector2((position.x + randomPosX), (position.y + randomPosY)) #housePos + Vector2(randomPosX, randomPosY)
+	path.add_child(newUnit)
+	worldPath.get_units()
+	
+	que -= 1
--- rts-game/Game/Menus/unit_spawn_menu.gd
+++ rts-game/Game/Menus/unit_spawn_menu.gd
@@ -1,8 +1,7 @@
 extends Node2D
 
-@onready var unit = preload("res://Faction1/Units/Test unit/unit.tscn")
-var housePos = Vector2()
 @export var unitCost = 10
+signal spawn
 
 func _process(delta: float) -> void:
 	if Game.resorces < unitCost:
@@ -11,15 +10,16 @@ func _process(delta: float) -> void:
 		$Yes.disabled = false
 
 func _on_yes_pressed() -> void:
-	var randomPosX = randi_range(-100, 100)
-	var randomPosY = randi_range(-100, 100)
-	
-	var path = get_tree().get_root().get_node("World/Friendly Units")
-	var worldPath = get_tree().get_root().get_node("World")
-	var newUnit = unit.instantiate()
-	newUnit.position = Vector2((housePos.x + randomPosX), (housePos.y + randomPosY)) #housePos + Vector2(randomPosX, randomPosY)
-	path.add_child(newUnit)
-	worldPath.get_units()
+	#var randomPosX = randi_range(-100, 100)
+	#var randomPosY = randi_range(-100, 100)
+	#
+	#var path = get_tree().get_root().get_node("World/Friendly Units")
+	#var worldPath = get_tree().get_root().get_node("World")
+	#var newUnit = unit.instantiate()
+	#newUnit.position = Vector2((housePos.x + randomPosX), (housePos.y + randomPosY)) #housePos + Vector2(randomPosX, randomPosY)
+	#path.add_child(newUnit)
+	#worldPath.get_units()
+	spawn.emit()
 	Game.resorces -= unitCost
 
 
